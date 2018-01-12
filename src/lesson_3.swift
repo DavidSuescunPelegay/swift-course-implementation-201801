4011a50d845cf1a9ee864105b07cad0c50f33fd7
David Suescun Pelegay <dsuescun96@gmail.com>
2018-01-12 20:20:31 +0100

    Added Lesson 3

---
+++ src/lesson_3.swift
@@ -0,0 +1,36 @@
+// Switch
+var name = "David";
+var nick = "D";
+var age = 21;
+let city = "Zaragoza";
+var change = true;
+let cars = {"Audi", "Mercedes", "BMW"};
+let carPrices = {"Audi": 30000, "Mercedes": 28000, "BMW": 35000}
+let position = (1,1);
+
+
+switch (age) {
+    case 21:
+        print("My age");
+
+    default:
+        print("Not my age");
+}
+
+
+switch (age) {
+    case 0..<35:
+        print("My age range");
+    default:
+        print("Not my age");
+}
+
+
+switch (position) {
+    case (1,1):
+        print("My current position");
+    default:
+        print("Not my current position");
+}
+
+(age < 18) ? print("Under aged") : print("Not under age") 
